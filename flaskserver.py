2c5225a527b47e83675d07da581d287e72933cae
sanskriti <sanskriti.kadam.0211@gmail.com>
2024-05-01 10:35:29 +0530

    committed now

--- flaskserver.py
+++ flaskserver.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
-app = Flask(__name__, static_folder='template', static_url_path='')
+app = Flask(__name__, static_folder='templates', static_url_path='')
 socketio = SocketIO(app)
 
 users = {}
