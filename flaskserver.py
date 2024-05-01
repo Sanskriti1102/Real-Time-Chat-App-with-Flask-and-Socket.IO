58f66939920e1384c9c95e585886b76b2bb116e6
sanskriti <sanskriti.kadam.0211@gmail.com>
2024-05-01 10:20:23 +0530

    committed now

---
+++ flaskserver.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__, static_folder='templates', static_url_path='')
+socketio = SocketIO(app)
+
+users = {}
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@socketio.on('connect')
+def handle_connect():
+    user_id = request.sid
+    users[user_id] = {'name': 'IP ' + request.remote_addr}
+    print(f"User {user_id} connected")
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    user_id = request.sid
+    del users[user_id]
+    print(f"User {user_id} disconnected")
+
+@socketio.on('message')
+def handle_message(data):
+    user_id = request.sid
+    emit('message', {'name': users[user_id]['name'], 'text': data['text']}, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000)
